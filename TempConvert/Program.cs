de65a2fa2f7a1a7beb8f553d52f099acb792b649
Francisco Caldeira <kikocaldeira2007@gmail.com>
2026-02-26 16:23:03 +0000

    Terceiro Commit

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -6,9 +6,19 @@ namespace TempConvert
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("A temperatura:");
+            float T=Convert.ToSingle(Console.ReadLine());
             Console.WriteLine("Escreva C para Celsius ou F para Fahrenheit");
             string temp=Console.ReadLine();
-            Console.WriteLine(temp);
+            switch (temp)
+            {
+                case "C":
+                Console.WriteLine($"A temperatura Celsius {T} em Fahrenheit é {T*1.8+32:f2}");
+                break;
+                case "F":
+                Console.WriteLine($"A temperatura Fahrenheit {T} em Celsius é {(T-32)/1.8:f2}");
+                break;
+            }
         }
     }
 }
